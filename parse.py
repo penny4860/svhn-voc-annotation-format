dc331a45463c44b72bcc047c8f2ab6a6fffd50cc
jeongjoonsup <penny4860@gmail.com>
2018-01-06 20:37:34 +0900

    get_box_text(), get_label_text() extraction

--- parse.py
+++ parse.py
@@ -15,6 +15,19 @@ def indent(elem, level=0):
         if level and (not elem.tail or not elem.tail.strip()):
             elem.tail = i
 
+
+def get_box_text(box):
+    x1 = str(int(box["left"]))
+    y1 = str(int(box["top"]))
+    x2 = str(int(box["left"] + box["width"]))
+    y2 = str(int(box["top"] + box["height"]))
+    return x1, y1, x2, y2
+
+def get_label_text(box):
+    label_str = str(int(box["label"]))
+    return label_str
+
+
 from xml.etree.ElementTree import Element, SubElement, tostring, dump
 json_data=open("imgs//annotation.json").read()
 anns = json.loads(json_data)
@@ -25,16 +38,12 @@ for ann in anns:
     fname.text = ann["filename"]
     
     for b in ann['boxes']:
-        x1 = str(int(b["left"]))
-        y1 = str(int(b["top"]))
-        x2 = str(int(b["left"] + b["width"]))
-        y2 = str(int(b["top"] + b["height"]))
-        label_str = str(int(b["label"]))
+        x1, y1, x2, y2 = get_box_text(b)
 
         obj = SubElement(root, "object")
         
         label = SubElement(obj, "name")
-        label.text = label_str
+        label.text = get_label_text(b)
         bndbox = SubElement(obj, "bndbox")
 
         xmin = SubElement(bndbox, "xmin")
